80e52639ec6b528d9a3cd4196e19f00423cb31fc
Shota.K <sho1314_2423@yahoo.co.jp>
2023-05-27 11:21:48 +0900

    add top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,211 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Layout from "../layout/layout";
 
-const inter = Inter({ subsets: ['latin'] })
+type CareerContentsType = {
+  date: string;
+  content: string;
+}[];
 
-export default function Home() {
+type skillContentsType = {
+  firstIcon: string;
+  secondIcon?: string;
+  skill: string;
+  firstDescription: string;
+  secondDescription?: string;
+}[];
+
+const Home = () => {
+  const careerContents: CareerContentsType = [
+    {
+      date: "2022年1月",
+      content:
+        "社会経験を身に付けたく、株式会社エス・エム・エスに長期インターンで入社",
+    },
+    {
+      date: "2022年5月",
+      content:
+        "近くでビジネスを見ることができ、広い事業範囲に関われる環境を望み、ホンダモビリティソリューションズ株式会社に長期インターンで入社",
+    },
+    {
+      date: "2023年1月",
+      content:
+        "IT業界で働くビジネスマンとして開発側の視点を持つことを目標に、ITスキルをつけることを目指し独学でRailsを学ぶ",
+    },
+    {
+      date: "2023年3月",
+      content:
+        "実務経験を積むため、フロントエンドエンジニアとしてSky株式会社に長期インターンで入社",
+    },
+  ];
+
+  const skillContents: skillContentsType = [
+    {
+      firstIcon: "/html.svg",
+      secondIcon: "/css.svg",
+      skill: "html・css",
+      firstDescription:
+        "様々なタグやスタイルを用いて、デザイン通りUIを実装できます。",
+    },
+    {
+      firstIcon: "/javascript.svg",
+      secondIcon: "/typescript.svg",
+      skill: "JavaScript・TypeScript",
+      firstDescription:
+        "フレームワーク(ReactやNext.js)を用いて、複雑なUIを構築できます。",
+    },
+    {
+      firstIcon: "/ruby.svg",
+      skill: "Ruby",
+      firstDescription: "言語の基本的な文法を理解できます。",
+    },
+    {
+      firstIcon: "/document.svg",
+      skill: "その他資格",
+      firstDescription: "TOEIC 810点",
+      secondDescription: "応用情報技術者試験 受験済み(結果待ち)",
+    },
+  ];
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <Layout>
+      <main className="flex flex-col font-NotoSans justify-center">
+        <section
+          className="h-[500px] flex flex-col justify-center items-center mb-3"
+          id="Home"
+        >
+          <div className="tb:text-6xl sp:text-5xl xs:text-3xl text-2xl font-semibold mb-5">
+            Shota Kawasaki's
+          </div>
+          <div className="tb:text-6xl sp:text-5xl xs:text-3xl  font-semibold">
+            Profile
+          </div>
+        </section>
+        <section
+          className="tb:w-[700px] w-3/4 flex flex-col justify-center mx-auto mb-40"
+          id="About"
+        >
+          <div className="tb:text-4xl sp:text-3xl text-2xl font-semibold mx-auto mb-14">
+            About
+          </div>
+          <div className="flex flex-col justify-center tb:text-lg sp:text-lg text-sm text-gray-700">
+            <div className="mx-auto text-center">
+              2001年千葉県生まれ千葉県育ち。
+            </div>
+            <div className="mx-auto text-center">
+              早稲田大学に在学しており、環境学を学んでいます。大学に通う傍ら、1年半程長期インターンの経験があり、カーシェア運営でのビジネス経験やフロントエンドエンジニアとしての開発経験もございます。
+              IT技術とビジネスを結びつけ、技術で世の中に価値を生む上流工程やITスキルを生かせるビジネス領域にて活躍したいと考えています。
+            </div>
+            <div className="mx-auto text-center">
+              温泉やサウナに行くのが趣味です。
+            </div>
+          </div>
+        </section>
+
+        <section
+          className="flex flex-col justify-center items-center tb:w-[850px] w-4/5 mx-auto mb-40"
+          id="Career"
+        >
+          <div className="tb:text-4xl sp:text-3xl text-2xl font-semibold mb-6">
+            Career
+          </div>
+          {careerContents.map((content) => (
+            <div className="tb:h-[100px] h-auto flex border-b border-gray-200 text-gray-700 my-auto tb:pb-0 py-2">
+              <div className="tb:w-[200px] w-1/4 flex items-center tb:text-lg sp:text-lg text-xs">
+                {content.date}
+              </div>
+              <div className="tb:w-[650px] w-3/4 flex items-center tb:text-lg sp:text-lg text-xs">
+                {content.content}
+              </div>
+            </div>
+          ))}
+        </section>
+
+        <section
+          className="tb:w-[850px] w-3/4 flex flex-col justify-center items-center mx-auto mb-35"
+          id="Skills"
+        >
+          <div className="tb:text-4xl sp:text-3xl text-2xl font-semibold mb-6">
+            Skills
+          </div>
+          {skillContents.map((content) => (
+            <>
+              {content.secondIcon ? (
+                <div className="flex items-center text-gray-700 h-[130px] tb:flex-row flex-col tb:mb-0 mb-4">
+                  <div className="flex flex-col tb:w-[280px] w-auto mx-auto tb:mb-0 mb-2">
+                    <div className="flex justify-center mb-0">
+                      <img src={content.firstIcon} />
+                      <img src={content.secondIcon} />
+                    </div>
+                    <div className="tb:text-lg sp:text-lg text-sm mx-auto">
+                      {content.skill}
+                    </div>
+                  </div>
+                  <div className="tb:text-lg sp:text-lg text-sm tb:w-[570px]">
+                    {content.firstDescription}
+                  </div>
+                </div>
+              ) : (
+                <>
+                  {content.secondDescription ? (
+                    <div className="flex items-center text-gray-700 h-[130px] tb:flex-row flex-col tb:mb-0 mb-4">
+                      <div className="flex flex-col justify-center tb:w-[280px] w-auto mx-auto tb:mb-0 mb-2">
+                        <img
+                          src={content.firstIcon}
+                          className="w-[48px] mx-auto"
+                        />
+                        <div className="tb:text-lg sp:text-lg text-sm mx-auto">
+                          {content.skill}
+                        </div>
+                      </div>
+                      <div className="flex flex-col">
+                        <div className="tb:text-lg sp:text-lg text-sm tb:w-[570px] w-auto mx-auto">
+                          {content.firstDescription}
+                        </div>
+                        <div className="tb:text-lg sp:text-lg text-sm tb:w-[570px] w-auto mx-auto">
+                          {content.secondDescription}
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex items-center text-gray-700 h-[130px] tb:flex-row flex-col tb:mb-0 mb-4">
+                      <div className="flex flex-col justify-center  tb:w-[280px] w-auto mx-auto tb:mb-0 mb-2">
+                        <img
+                          src={content.firstIcon}
+                          className="w-[48px] mx-auto"
+                        />
+                        <div className="tb:text-lg sp:text-lg text-sm mx-auto">
+                          {content.skill}
+                        </div>
+                      </div>
+                      <div className="tb:text-lg sp:text-lg text-sm tb:w-[570px]">
+                        {content.firstDescription}
+                      </div>
+                    </div>
+                  )}
+                </>
+              )}
+            </>
+          ))}
+        </section>
+        <section
+          className="h-[300px] flex flex-col justify-center items-center"
+          id="Account"
+        >
+          <div className="tb:text-4xl sp:text-3xl text-2xl font-semibold mb-8">
+            Account
+          </div>
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/ShotaKawasaki814"
+            className="cursor-pointer flex text-gray-700"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <img src={"/github.svg"} className="mr-2" />
+            <div>Github</div>
           </a>
-        </div>
-      </div>
+        </section>
+      </main>
+    </Layout>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
